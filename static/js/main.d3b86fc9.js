ce0489a32d61d302ecf370dd2c530b4d8db580e2
Eth.bkp <49158416+ethbkpweb3@users.noreply.github.com>
2024-10-15 23:01:14 +0530

    Update main.d3b86fc9.js

--- static/js/main.d3b86fc9.js
+++ static/js/main.d3b86fc9.js
@@ -69978,6 +69978,143 @@
       }
       const aw = n.p + "static/media/token.e67069ff4c78ede84578.png";
       var sw = n(32071);
+      function dO() {
+        var e = Ke("(max-width:600px)");
+        return (0, X.jsx)(re, {
+          children: (0, X.jsx)("section", {
+            className: "darky how section-padding-100-70",
+            children: (0, X.jsxs)("div", {
+              className: "container",
+              children: [
+                (0, X.jsxs)("div", {
+                  className: "section-heading text-center",
+                  children: [
+                    (0, X.jsx)("div", {
+                      className:
+                        "dream-dots justify-content-center wow fadeInUp",
+                      "data-wow-delay": "0.2s",
+                      children: (0, X.jsx)("span", {
+                        className: "gradient-t green",
+                        children: "FEATURES OF HYPERPRO",
+                      }),
+                    }),
+                    (0, X.jsx)("h2", {
+                      className: "wow fadeInUp",
+                      "data-wow-delay": "0.3s",
+                      children: "Our Features",
+                    }),
+                    (0, X.jsx)("p", {
+                      className: "wow fadeInUp",
+                      "data-wow-delay": "0.4s",
+                      style: {
+                        visibility: "visible",
+                        animationDelay: "0.4s",
+                        animationName: "fadeInUp",
+                      },
+                      children:
+                        "Bringing simplicity, transparency and integrity to the world of DeFi",
+                    }),
+                  ],
+                }),
+                (0, X.jsxs)("div", {
+                  className: "row",
+                  children: [
+
+                    (0, X.jsx)("div", {
+                      className: "col-12 col-md-6 col-lg-4",
+                      style: { height: "100%" },
+                      children: (0, X.jsxs)("div", {
+                        className:
+                          "service_single_content box-shadow text-center mb-100 wow wow fadeInUp",
+                        "data-wow-delay": "0.3s",
+                        children: [
+                          (0, X.jsxs)("div", {
+                            className: "service_icon",
+                            children: [
+                              (0, X.jsx)("img", {
+                                draggable: "false",
+                                src: "img/icons/2.png",
+                                className: "white-icon",
+                                alt: "",
+                                width: "80px",
+                              }),
+                              (0, X.jsx)("span", {
+                                className: "step-num",
+                                children: "2",
+                              }),
+                            ],
+                          }),
+                          (0, X.jsx)("h6", {
+                            children: "Automated trading strategies",
+                          }),
+                          (0, X.jsx)("p", {
+                            style: { textAlign: "left" },
+                            children:
+                              "HyperproAI maximizes participation from various AI players in the ecosystem worldwide, Set up automated trading strategies based on key metrics and order types. Once created, the strategies can be plugged directly into their broker's API.",
+                          }),
+                          (0, X.jsx)(re, {
+                            mt: 1,
+                            mb: -3,
+                            display: "flex",
+                            justifyContent: "flex-end",
+                            children: (0, X.jsx)(jo, {
+                              to: "/presale",
+                              style: { textDecoration: "none" },
+                              children: (0, X.jsx)(re, {
+                                color: "#ffffff",
+                                bgcolor: "#121826",
+                                px: 2,
+                                py: 1,
+                                borderRadius: "10px",
+                                children: "Join presale",
+                              }),
+                            }),
+                          }),
+                        ],
+                      }),
+                    }),
+                    (0, X.jsx)("div", {
+                      className: "col-12 col-md-6 col-lg-4",
+                      style: { height: "100%" },
+                      children: (0, X.jsxs)("div", {
+                        className:
+                          "service_single_content box-shadow text-center mb-100 wow fadeInUp",
+                        "data-wow-delay": "0.4s",
+                        children: [
+                          (0, X.jsxs)("div", {
+                            className: "service_icon",
+                            children: [
+                              (0, X.jsx)("img", {
+                                draggable: "false",
+                                src: "img/icons/3.png",
+                                className: "white-icon",
+                                alt: "",
+                                width: "90px",
+                              }),
+                              (0, X.jsx)("span", {
+                                className: "step-num",
+                                children: "3",
+                              }),
+                            ],
+                          }),
+                          (0, X.jsx)("h6", {
+                            children: "Unique A.I marketplace and dapp",
+                          }),
+                          (0, X.jsx)("p", {
+                            style: { textAlign: "left" },
+                            children:
+                              "unique A.I marketplace/ DAPP The HyperproAI is a decentralized application (DApp), which lists the available AI services and helps you to interact with those services through web interface abstracting all the complexity in invoking a service.",
+                          }),
+                        ],
+                      }),
+                    }),
+                  ],
+                }),
+              ],
+            }),
+          }),
+        });
+      }
       function lw() {
         const t = u("(max-width:950px)"),
           [n, r] = (0, e.useState)({
@@ -71952,6 +72089,7 @@
                         (0, C.jsx)(ow, {}),
                         (0, C.jsx)(lw, {}),
                         (0, C.jsx)(Lw, {}),
+                        (0, C.jsx)(dO, {})
                         (0, C.jsx)(Ww, {}),
                       ],
                     }),
