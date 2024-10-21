e25985ae89c609c9542d3d49523b69bedcae9fd0
Eth.bkp <49158416+ethbkpweb3@users.noreply.github.com>
2024-10-22 03:32:52 +0530

    0

--- static/js/main.d3b86fc9.js
+++ static/js/main.d3b86fc9.js
@@ -68570,7 +68570,40 @@
                                       " ",
                                 
                                     ],
-                                  }),
+                                  }),              ( C.jsx)(O, {
+                                    display: "flex",
+                                    justifyContent: "center",
+                                    mt: 2,
+                                    children: (0,
+                                    C.jsxs)(zt, {
+                                        href: "/how-to-buy",
+                                        target: "_blank",
+                                        sx: {
+                                            width: t ? "80%" : "60%",
+                                            height: "32px",
+                                            border: "2px solid #4B4C59",
+                                            background: "#414141",
+                                            borderRadius: "8px",
+                                            display: "flex",
+                                            alignItems: "center",
+                                            justifyContent: "center",
+                                            color: "#fff",
+                                            fontSize: t ? "12px" : "13px",
+                                            cursor: "pointer",
+                                            textTransform: "capitalize",
+                                            "&:hover": {
+                                                background: "#414141"
+                                            }
+                                        },
+                                        children: [(0,
+                                        C.jsx)(Ty, {
+                                            style: {
+                                                fontSize: "20px",
+                                                marginRight: "7px"
+                                            }
+                                        }), " ", "Add Base Network"]
+                                    })
+                                }),
                                   (0, C.jsxs)(O, {
                                     sx: {
                                       position: "relative",
